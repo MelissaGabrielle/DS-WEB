c361f1af1efcdcda48825005abbc3b33aac13781
MELISSA GABRIELLE ALVES VIEIRA DA SILVA <melissa.silva70@senaisp.edu.br>
2026-02-03 16:04:40 -0300

    Atualizando repositório com atividades POO

---
+++ PBE/Aula-01/atividade-01.php
@@ -0,0 +1,133 @@
+<?php
+
+// CLASSE ESMALTE
+class Esmalte {
+
+    //Atributo
+    public $tamanho;
+    public $cor;
+    public $formato;
+    public $tipagem;
+    public $marca; 
+
+    //Método
+    public function pintar() { 
+
+    }
+    
+    //Método
+    public function palitar() { 
+
+    }
+    
+    //Método
+    public function remover() { 
+
+    }
+}
+
+// CLASSE GLOSS
+class Gloss {
+
+    //Atributo
+    public $tamanho;
+    public $cor;
+    public $formato;
+    public $cheiro;
+    public $marca; 
+
+    //Método
+    public function fechar() { 
+
+    }
+    
+    //Método
+    public function aplicar() { 
+
+    }
+    
+    //Método
+    public function abrir() { 
+
+    }
+}
+
+// CLASSE RÌMEL
+class Esmalte {
+
+    //Atributo
+    public $textura;
+    public $cor;
+    public $formato;
+    public $tipagem;
+    public $marca; 
+
+    //Método
+    public function abrir() { 
+
+    }
+    
+    //Método
+    public function aplicar() { 
+
+    }
+    
+    //Método
+    public function tirar_excesso() { 
+
+    }
+}
+
+// CLASSE SECADOR DE CABELO
+class SecadorCabelo {
+
+    //Atributo
+    public $cor;
+    public $temperatura;
+    public $velocidade;
+    public $voltagem;
+    public $marca; 
+
+    //Método
+    public function ligar() { 
+
+    }
+    
+    //Método
+    public function ajustar_temperatura() { 
+
+    }
+    
+    //Método
+    public function modo_frio() { 
+
+    }
+}
+
+// CLASSE BOLSA
+class Bolsa {
+
+    //Atributo
+    public $tamanho;
+    public $cor;
+    public $itens_dentro;
+    public $material;
+    public $marca; 
+
+    //Método
+    public function abrir() { 
+
+    }
+    
+    //Método
+    public function fechar() { 
+
+    }
+    
+    //Método
+    public function colocar_itens() { 
+
+    }
+}
+
+?>
---
+++ PBE/index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "<h1>Aula de Programação Orientada a Objetos</h1>"
+
+?>
